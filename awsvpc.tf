ca8923a42f9c782e67802596a8168930b7c7ad00
Chris Beck <cbeck@hashicorp.com>
2022-05-17 21:55:24 -0400

    .

--- awsvpc.tf
+++ awsvpc.tf
@@ -93,11 +93,6 @@ resource "aws_route_table" "rt" {
     gateway_id = aws_internet_gateway.gw.id
   }
 
-  route {
-    cidr_block = hcp_hvn.vault-hvn.cidr_block
-    transit_gateway_id = hcp_aws_transit_gateway_attachment.vault-hcp-tgwa.provider_transit_gateway_attachment_id
-  }
-
   tags = {
     Name = "cbeck_rt"
   }
